3e4ab76454ff72b5094dd20e4169baa74aa9a566
nishanthnirujogi <nirujoginisanth@gmail.com>
2025-01-14 16:51:35 +0530

    able to open FB login

--- tiktokscrapper.py
+++ tiktokscrapper.py
@@ -1,10 +1,11 @@
-import selenium
 from selenium import webdriver
-print('Imported Selenium Webdriver')
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 from selenium.webdriver.chrome.service import Service
 path = "C:/Users/nirujogi/Desktop/selenium/chromedriver.exe"
 s = Service(path)
 driver = webdriver.Chrome(service=s)
 driver.get("https://www.facebook.com/login/")
-
+time.sleep(10)
